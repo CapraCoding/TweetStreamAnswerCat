66b7ad1fac4ff199d0df64864abcdccb1366e134
CapraCoding <113940238+CapraCoding@users.noreply.github.com>
2023-02-15 16:42:06 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+#Troque pela sua chave de API Twitter Dev
+consumer_key = "CONSUMER_KEY"
+consumer_secret = "CONSUMER_SECRET"
+access_token = "ACESS_TOKEN"
+access_token_secret = "ACESS_TOKEN_SECRET"
